f7d95b325ce860247f7cd25816d0f5d20fbb7ca8
xiaomi <1090773861@qq.com>
2018-12-13 11:28:09 +0800

    玩

---
+++ bai/index.js
@@ -0,0 +1,14 @@
+// // 标题输入框
+// var input = document.querySelector(".editor_textfield").value;
+
+// input = "瑞津国际 官方导师微信ye06096";
+
+
+// //img有图片才显示
+// var img = document.querySelector(".BDE_Image");
+
+	
+  
+  document.querySelector(".editor_textfield").value = "瑞津国际 官方导师微信ye06096";
+  // document.querySelector(".edui-editor-body .BDE_Image").src="./img/428872414552108257.jpg";
+  document.querySelector(".btn_middle").click();
